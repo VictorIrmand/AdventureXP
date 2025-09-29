563d481c985594b3a399f4820a9b64240c333cac
VictorIrmand <vini0003@stud.kea.dk>
2025-09-29 12:38:45 +0200

    Initial commit

---
+++ src/main/java/org/example/adventurexp/AdventureXpApplication.java
@@ -0,0 +1,13 @@
+package org.example.adventurexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureXpApplication.class, args);
+    }
+
+}
