4291dfed68d96b4f58b5d76c24b61d8b94226205
rasbiehl <rasbiehl@gmail.com>
2025-10-09 23:07:40 +0200

    der er ryddet rigtig meget op i exception handling i userflow

---
+++ src/main/java/org/example/adventurexp/exception/DatabaseAccessException.java
@@ -0,0 +1,7 @@
+package org.example.adventurexp.exception;
+
+public class DatabaseAccessException extends RuntimeException {
+  public DatabaseAccessException(String message) {
+    super(message);
+  }
+}
